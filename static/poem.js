e1c27bf0ba3392e9f2e03b54ae08327bbbab572c
auual <ding@ibyte.me>
2022-02-20 17:10:21 +0800

    frist commit.

---
+++ static/poem.js
@@ -0,0 +1,7 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/highlight.min.js"></script>
+
+<script src="https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/5.0.2/js/bootstrap.bundle.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/languages/rust.min.js"></script>
+
+hljs.initHighlightingOnLoad();
+console.log("If you find a bug on this page, please contact the developer <ding@ibyte.me>");
